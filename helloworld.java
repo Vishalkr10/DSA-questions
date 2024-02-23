16a63c88b34fc0f2a771f0cf4249e13cfcf4319a
vishal kumar <vishalradhanagar7654@gmail.com>
2024-02-24 00:47:49 +0530

    push the code

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class helloworld{
+    public static void main(String[] args) {
+        System.out.println("hey! ");
+        System.out.println("welcome to the world of data structures and algrithm");
+    }
+}
